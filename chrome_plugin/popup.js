d18af2baf828d4b32678f409ff599209eb962aa5
52muffin <myosotishy@duksung.ac.kr>
2025-04-07 16:19:43 +0900

    크롬 플러그인-3

--- chrome_plugin/popup.js
+++ chrome_plugin/popup.js
@@ -4,12 +4,32 @@ document.getElementById("extract").addEventListener("click", () => {
     });
 });
 
-
 document.getElementById("details").addEventListener("click", () => {
     chrome.tabs.create({ url: chrome.runtime.getURL("details.html") });
 });
 
-
+document.getElementById("analyze").addEventListener("click", () => {
+    chrome.storage.local.get("extractedData", (result) => {
+        if (result.extractedData) {
+            fetch("http://127.0.0.1:8000/receive/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(result.extractedData)
+            })
+            .then(res => res.json())
+            .then(data => {
+                alert("서버 응답: " + JSON.stringify(data));
+            })
+            .catch(err => {
+                alert("전송 중 에러 발생: " + err);
+            });
+        } else {
+            alert("먼저 기사를 추출하세요!");
+        }
+    });    
+});
 
 document.getElementById("close").addEventListener("click", () => {
     window.close();
